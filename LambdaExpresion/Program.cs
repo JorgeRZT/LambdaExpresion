7659090aedb7754bff51cf02490f765a0a9d7c72
JorgeRZT <georgeforceone@gmail.com>
2017-08-03 12:23:24 +0200

    Agregar archivos de proyecto.

---
+++ LambdaExpresion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LambdaExpresion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>()
+            {
+                1,2,3,4,5,6,7,8,9
+            };
+
+            //Expresion
+            numbers.Where(n => n > 1);
+        }
+    }
+}
